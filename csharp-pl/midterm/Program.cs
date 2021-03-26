b6538fd71260b1a4abee99df16d1737741f1b161
Aco <40295780+CXL58APj@users.noreply.github.com>
2021-03-26 20:47:48 +0800

    added csharp-pl
    has validation

---
+++ csharp-pl/midterm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeric = null;
+            string delimiter = null;
+            int length = 0;
+            double sum = 0, average = 0;
+            Console.Write("Enter delimited numeric string: ");
+            numeric = Console.ReadLine();
+            Console.Write("Enter delimiter: ");
+            delimiter = Console.ReadLine();
+            string[] arrayList = numeric.Split(delimiter);
+            System.Console.WriteLine("--------------------");
+            foreach (var _numeric in arrayList){
+                System.Console.WriteLine(_numeric);
+                length++;
+                }
+            System.Console.WriteLine("--------------------");
+              for(int i=0; i<length ; i++)
+              {
+                  sum += Convert.ToDouble (arrayList[i]);
+                  }
+                  average = sum / length ;
+                  System.Console.WriteLine(average ) ;
+        }
+    }
+}
